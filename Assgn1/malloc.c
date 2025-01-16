c279573086ff1c6d4a43906423b6ce4fdc9c8649
leah16303 <leahsarles@gmail.com>
2025-01-15 22:55:11 -0800

    passing 42/47 tests

--- Assgn1/malloc.c
+++ Assgn1/malloc.c
@@ -1,8 +1,17 @@
 #include "malloc.h"
 #define ALLIGN 16
+#define DEBUG_BUF 256
 
 /*first header in the list of headers*/
 header* start = NULL;
+
+
+/* Helper function to print debugging messages */
+void debug_print(const char *msg) {
+    if (getenv("DEBUG_MALLOC")) {  /*check if DEBUG_MALLOC is set*/ 
+        fputs(msg, stderr);  /*print message to stderr*/ 
+    }
+}
 
 /*calloc allocates memory for nmemb elements of size "size" bytes*/
 /*clear allocated block so that there is no junk*/
@@ -35,6 +44,17 @@ void *calloc(size_t nmemb, size_t size){
 
         for (i = 0; i < block_size; i++){
             ((char *)block_data)[i] = '\0';
+        }
+
+        /* debug */
+        if (getenv("DEBUG_MALLOC")) {
+            char msg[DEBUG_BUF];
+            void *data = block_data;
+            size_t tsize = total_size;
+            unsigned int msgsize = sizeof(msg);
+            snprintf(msg,msgsize,"MALLOC: calloc(%zu,%zu) =>", nmemb, size);
+            snprintf(msg,msgsize, "(ptr=%p, size=%zu)\n", data, tsize);
+            debug_print(msg);
         }
 
         return block_data;
@@ -76,14 +96,20 @@ void *malloc(size_t size){
     /*if a free block is found, change block to not free*/
     if (new_block != NULL){
         new_block->is_free = 0;
-
-        
-
-
         size_t header_size = align(sizeof(header));
 
          /*returns pointer memory after header*/
         void *block_data = (void *)((uintptr_t)new_block + header_size);
+
+        if (getenv("DEBUG_MALLOC")) {
+            char msg[100];
+            void *data = block_data;
+            size_t newsize = new_block->size;
+            unsigned int msgsize = sizeof(msg);
+            snprintf(msg, msgsize, "MALLOC: malloc(%zu) =>", size);
+            snprintf(msg, msgsize, "(ptr=%p, size=%zu)\n",data, newsize);
+            debug_print(msg);
+        } 
         return block_data;
     }
     else{/*if no free block is not found, allocate a new block*/
@@ -129,11 +155,11 @@ void *find_free_block(size_t size){
 /*note that if size is not divisble by 16, */
 /*memory will be added so that is it dvisible by 16*/
 /*returns a pointer to the memory after the header*/
-void *allocate_block(size_t requested_memory_size){
+void *allocate_block(size_t mem_size){
     header *new_block;
 
     size_t header_size = align(sizeof(header));
-    size_t block_size = align(requested_memory_size);
+    size_t block_size = align(mem_size);
     size_t total_size = header_size + block_size;
 
 
@@ -169,6 +195,16 @@ void *allocate_block(size_t requested_memory_size){
 
     /*returns pointer memory after header*/
     void *block_data = (void *)((uintptr_t)new_block + header_size);
+
+
+    if (getenv("DEBUG_MALLOC")) {
+            char msg[DEBUG_BUF];
+            void *data = block_data;
+            unsigned int msgsize = sizeof(msg);
+            snprintf(msg, msgsize, "MALLOC: malloc(%zu) =>", mem_size);
+            snprintf(msg, msgsize, "(ptr=%p, size=%zu)\n",data, total_size);
+            debug_print(msg);
+        } 
     return block_data;
 }
 
@@ -198,6 +234,12 @@ void free(void *ptr){
     remove_free_blocks();
     /*merge remaining free blocks together*/
     merge_blocks();
+
+    if (getenv("DEBUG_MALLOC")) {
+        char msg[DEBUG_BUF];
+        snprintf(msg, sizeof(msg), "MALLOC: free(%p)\n", ptr);
+        debug_print(msg);
+    }
     return;
 
 }
@@ -232,7 +274,6 @@ int remove_free_blocks(){
 
         if (current == NULL){ /*make start = NULL*/
             start = NULL;
-            start->next = NULL;
         }
         else{
             current->next = NULL;
@@ -269,14 +310,11 @@ void *realloc(void *ptr, size_t size){
     size_t header_size = align(sizeof(header));
     size_t block_size = align(size);
     size_t total_size = header_size + block_size;
+    void *retptr;
 
     /*if ptr and size are both NULL, return NULL*/
     if (ptr == NULL && size == 0){
         return NULL;
-    }
-    /*if ptr is NULL call malloc*/
-    else if (ptr == NULL){
-        return malloc(size);
     }
     /*if size = 0, deallocate the memory at given ptr*/
     else if (size == 0){
@@ -287,32 +325,43 @@ void *realloc(void *ptr, size_t size){
     
     /*find the pointer in the lists of pointers*/
     header *block = free_helper(ptr);
-    
-    if (block == NULL){
-        return NULL;
+    if (block == NULL){ /*block does not exist --> malloc*/
+        return malloc(size);
     }
 
     /*if the size requested is smaller than*/
     /*the current size, return the same ptr*/
     if (block->size >= total_size){
         block->size = total_size;
-        return ptr;
+        retptr = ptr;
     }
     /*if the size requested is larger than the current size*/
     /*allocate a new block of memory and copy the data*/
     else{
 
-        /*check if next block is free and if so merge it*/
+        int make_new_block = 0;
+        /*check if next block is free*/
         if (block->next != NULL && block->next->is_free){
-            block->size += block->next->size;
-            block->next = block->next->next;
-            if (block->next != NULL){
+            /*check if blocks were merged the size would be large enough*/
+            if (block->size + block->next->size >= total_size){
+                block->size = total_size;
+                block->next = block->next->next;
+                if (block->next != NULL){
                 /*set the new next blocks prev to current block*/
                 block->next->prev = block;
+                }
+                retptr = ptr;
             }
-            return ptr;
+            else{
+                make_new_block = 1;
+            }
+            
         }
         else{
+            make_new_block = 1;
+        }
+
+        if (make_new_block){
             void *new_ptr = malloc(size);
             if (new_ptr == NULL){
                 return NULL;
@@ -321,11 +370,22 @@ void *realloc(void *ptr, size_t size){
             memcpy(new_ptr, ptr, block->size);
             free(ptr);
             merge_blocks();
-            return new_ptr;
+            retptr = new_ptr;
         }
         
         
     }
+
+    if (getenv("DEBUG_MALLOC")) {
+            char msg[DEBUG_BUF];
+            size_t tsize = total_size;
+            unsigned int msgsize = sizeof(msg);
+            snprintf(msg,msgsize,"MALLOC: realloc(%p,%zu) =>", retptr, size);
+            snprintf(msg,msgsize, "(ptr=%p, size=%zu)\n", retptr, tsize);
+            debug_print(msg);
+        }
+
+    return retptr;
 }
 
 /*finds the header that corresponds to the given ptr*/
@@ -344,8 +404,7 @@ void *free_helper(void *ptr){
 
 /*returns a size that is divisible by alignment*/
 size_t align(size_t size){
-    size_t allignment = (ALLIGN - (size % ALLIGN));
-    return size + allignment;
+    return (size + ALLIGN - 1) & ~(ALLIGN - 1);
 }
 
 
